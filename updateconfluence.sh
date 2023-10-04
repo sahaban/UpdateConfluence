cdaee9468d8025ac0273b2b559f909a0c8d4c25a
sahaban <blackiron629@gmail.com>
2023-10-05 04:33:40 +0530

    update jenkinsfile

--- updateconfluence.sh
+++ updateconfluence.sh
@@ -4,7 +4,7 @@ confluenceUser="emv52697@zslsz.com"
 confluencePass=":ATATT3xFfGF0tcjYKcj1BzqTH68_mopLKmo8N710lpWKQGpI-WPBBp8pPUkOp3gvANo-gYAbFUeW9ISmq1FXYZfPH23AvQ7YIgrSOmdUKPR_B6sWB36qvbZA2Jk8oOrXkmeWrsL8GcS5qt2LuID8S1ScdK3zg-0j31VHVXx1FVVxODnzCLdhWnA=3634A4ED"
 
 #get current whole page content from confluence
-currentContent=`curl -s -u ${confluenceUser}:${confluencePass} -X GET https://blackiron629.atlassian.net/wiki/rest/api/content/33118?expand=body.storage | python -c "import sys, json; print json.load(sys.stdin)['body']['storage']['value']"`
+currentContent=`curl -s -u ${confluenceUser}:${confluencePass} -X GET https://blackiron629.atlassian.net/wiki/rest/api/content/33118?expand=body.storage | python3 -c "import sys, json; print json.load(sys.stdin)['body']['storage']['value']"`
 #If you manually modify the confluence page it will introdure few junk characters like "colspan="1"". Hence we are replacing them with blank
 Old=' colspan="1"'
 New=''
@@ -16,7 +16,7 @@ oldRow="</tbody></table>"
 newRow=$1
 newContent="${currentContent/$oldRow/$newRow}"
 
-pageVersion=`curl -s -u ${confluenceUser}:${confluencePass} -X GET https://alm-confluence.systems.uk.hsbc/confluence/rest/api/content/1396984912?expand=version | python -c "import sys, json; print json.load(sys.stdin)['version']['number']"+1`
+pageVersion=`curl -s -u ${confluenceUser}:${confluencePass} -X GET https://blackiron629.atlassian.net/wiki/rest/api/content/33118?expand=version | python3 -c "import sys, json; print json.load(sys.stdin)['version']['number']"+1`
 echo "pageVersion: ${pageVersion}"
 
 #Update the page with new table  row
