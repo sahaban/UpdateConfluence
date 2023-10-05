5b0da91abe6113e1b4fbbcf338bd772ef8764786
sahaban <74941524+sahaban@users.noreply.github.com>
2023-10-05 22:52:37 +0530

    Update updateconfluence.sh

--- updateconfluence.sh
+++ updateconfluence.sh
@@ -4,7 +4,7 @@ confluenceUser="confluence-username"
 confluencePass="confluence-user-passowrd"
 
 #get current whole page content from confluence
-currentContent=`curl -s -u ${confluenceUser}:${confluencePass} -X GET https://blackiron629.atlassian.net/wiki/rest/api/content/33118?expand=body.storage | python3 -c "import sys, json; print json.load(sys.stdin)['body']['storage']['value']"`
+currentContent=`curl -s -u ${confluenceUser}:${confluencePass} -X GET https://blackiron629.atlassian.net/wiki/rest/api/content/33118?expand=body.storage | python3 -c "import sys, json; print (json.load(sys.stdin)['body']['storage']['value'])"`
 #If you manually modify the confluence page it will introdure few junk characters like "colspan="1"". Hence we are replacing them with blank
 Old=' colspan="1"'
 New=''
@@ -16,7 +16,9 @@ oldRow="</tbody></table>"
 newRow=$1
 newContent="${currentContent/$oldRow/$newRow}"
 
-pageVersion=`curl -s -u ${confluenceUser}:${confluencePass} -X GET https://blackiron629.atlassian.net/wiki/rest/api/content/33118?expand=version | python3 -c "import sys, json; print json.load(sys.stdin)['version']['number']"+1`
+pageVersion=`curl -s -u ${confluenceUser}:${confluencePass} -X GET https://blackiron629.atlassian.net/wiki/rest/api/content/33118?expand=version | python3 -c "import sys, json; print (json.load(sys.stdin)['version']['number'])"`
+
+pageVersion=`expr $pageVersion + 1`
 echo "pageVersion: ${pageVersion}"
 
 #Update the page with new table  row
